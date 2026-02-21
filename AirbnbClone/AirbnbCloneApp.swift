0ed6b5539c4b46fc4a55e745f67424eb206d683b
Likhit Garimella <likhitgmella@gmail.com>
2026-02-21 16:56:11 -0500

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Likhit Garimella on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirbnbClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirbnbClone
+//
+//  Created by Likhit Garimella on 2/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
