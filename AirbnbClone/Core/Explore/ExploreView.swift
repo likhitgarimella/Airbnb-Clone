5f4d89bfb007a8ee34fc4e7233074b0e56713c18
Likhit Garimella <likhitgmella@gmail.com>
2026-02-21 17:27:30 -0500

    Explore View UI

---
+++ AirbnbClone/Core/Explore/ExploreView.swift
@@ -0,0 +1,29 @@
+//
+//  ExploreView.swift
+//  AirbnbClone
+//
+//  Created by Likhit Garimella on 2/21/26.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { listing in
+                        ListingItemView()
+                            .frame(height: 400)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
