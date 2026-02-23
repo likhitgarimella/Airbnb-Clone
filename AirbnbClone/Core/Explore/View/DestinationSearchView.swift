90034da2c4783d7babe5cf43850bfbb2d28579aa
Likhit Garimella <likhitgmella@gmail.com>
2026-02-23 01:52:15 -0500

    Listing Search Functionality

--- AirbnbClone/Core/Explore/View/DestinationSearchView.swift
+++ AirbnbClone/Core/Explore/View/DestinationSearchView.swift
@@ -16,7 +16,8 @@ enum DestinationSearchOptions {
 struct DestinationSearchView: View {
     
     @Binding var show: Bool
-    @State private var destination = ""
+    @ObservedObject var viewModel: ExploreViewModel
+    
     @State private var selectedOption: DestinationSearchOptions = .location
     @State private var startDate = Date()
     @State private var endDate = Date()
@@ -27,6 +28,7 @@ struct DestinationSearchView: View {
             HStack {
                 Button {
                     withAnimation(.snappy) {
+                        viewModel.updateListingsForLocation()
                         show.toggle()
                     }
                 } label: {
@@ -37,9 +39,10 @@ struct DestinationSearchView: View {
                 
                 Spacer()
                 
-                if !destination.isEmpty {
+                if !viewModel.searchLocation.isEmpty {
                     Button("Clear") {
-                        destination = ""
+                        viewModel.searchLocation = ""
+                        viewModel.updateListingsForLocation()
                     }
                     .foregroundStyle(.black)
                     .font(.subheadline)
@@ -59,8 +62,12 @@ struct DestinationSearchView: View {
                         Image(systemName: "magnifyingglass")
                             .imageScale(.small)
                         
-                        TextField("Search destinations", text: $destination)
+                        TextField("Search destinations", text: $viewModel.searchLocation)
                             .font(.subheadline)
+                            .onSubmit {
+                                viewModel.updateListingsForLocation()
+                                show.toggle()
+                            }
                     }
                     .frame(height: 44)
                     .padding(.horizontal)
@@ -139,7 +146,7 @@ struct DestinationSearchView: View {
 }
 
 #Preview {
-    DestinationSearchView(show: .constant(false))
+    DestinationSearchView(show: .constant(false), viewModel: ExploreViewModel(service: ExploreService()))
 }
 
 struct CollapsibleDestinationViewModifier: ViewModifier {
--- AirbnbClone/Core/Explore/ViewModel/ExploreViewModel.swift
+++ AirbnbClone/Core/Explore/ViewModel/ExploreViewModel.swift
@@ -10,7 +10,9 @@ internal import Combine
 
 class ExploreViewModel: ObservableObject {
     @Published var listings = [Listing]()
+    @Published var searchLocation = ""
     private let service: ExploreService
+    private var listingsCopy = [Listing]()
     
     init(service: ExploreService) {
         self.service = service
@@ -21,8 +23,18 @@ class ExploreViewModel: ObservableObject {
     func fetchListings() async {
         do {
             self.listings = try await service.fetchListings()
+            self.listingsCopy = listings
         } catch {
             print("DEBUG: Failed to fetch listings with error: \(error.localizedDescription)")
         }
+    }
+    
+    func updateListingsForLocation() {
+        let filteredListings = listings.filter({
+            $0.city.lowercased() == searchLocation.lowercased() ||
+            $0.state.lowercased() == searchLocation.lowercased()
+        })
+        
+        self.listings = filteredListings.isEmpty ? self.listingsCopy : filteredListings
     }
 }
