592fa822e75bec7e119d69e0f35e7de44527231b
Likhit Garimella <likhitgmella@gmail.com>
2026-02-23 00:58:45 -0500

    Listing Map View

---
+++ AirbnbClone/Extensions/Regions.swift
@@ -0,0 +1,14 @@
+//
+//  Regions.swift
+//  AirbnbClone
+//
+//  Created by Likhit Garimella on 2/23/26.
+//
+
+import Foundation
+import CoreLocation
+
+extension CLLocationCoordinate2D {
+    static var losAngeles = CLLocationCoordinate2D(latitude: 34.0549, longitude: -118.2426)
+    static var miami = CLLocationCoordinate2D(latitude: 25.7602, longitude: -80.1959)
+}
